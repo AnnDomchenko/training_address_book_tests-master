d057319afa807ed40af84736d08e73dd383c2ce4
AnnDomchenko <you@example.com>
2017-05-13 13:06:06 +0300

    delete_all_groups_non_working

--- tests/test_delete_all_groups.py
+++ tests/test_delete_all_groups.py
@@ -1,16 +1,14 @@
 import pytest
 import random
 
-@pytest.fixture(params=[0, "random", -1], ids=["first", "random", "last"])
-def index(app, request):
-    if request.param=="random":
-        return random.randrange(1, app.group.count())
-    return request.param
+# @pytest.fixture(params=[0, "random", -1], ids=["first", "random", "last"])
+# def index(app, request):
+#     if request.param=="random":
+#         return random.randrange(1, app.group.count())
+#     return request.param
 
-def test_delete_all_groups(app, init_login, init_group, index):
+def test_delete_all_groups(app):
     app.group.open_group_page()
     total_groups=app.group.count()
-    while total_groups != 0:
-        app.group.delete_by_number(index)
-        assert "Group has been removed." in app.find_message()
-        app.group.return_to_group_page()
+    app.group.delete_groups()
+    assert total_groups is None
